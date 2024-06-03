f0eef94c4a848fd8792cd77e9c773446ab8da440
Aditya <as0742004@gmail.com>
2024-06-03 15:49:29 +0530

    adding useEffect for server call

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect,useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,10 +7,15 @@ import { Todo } from './components/Todos'
 
 function App() {
   const [todos, settodos] = useState([]);
-  fetch("http://localhost:3000/todos").then( async function(res){
-    const json = await res.json();
-    settodos(json.todos);
-  })
+
+  useEffect(function () {
+    fetch("http://localhost:3000/todos").then(async function (res) {
+      const json = await res.json();
+      settodos(json.todos);
+    })
+
+  }, [])
+
 
   return (
     <div>
--- Frontend/src/components/Todos.jsx
+++ Frontend/src/components/Todos.jsx
@@ -3,7 +3,7 @@
 export function Todo({ todos }) {
     return <div>
         {todos.map(function (todo) {
-            return <div>
+            return <div key={todo._id}>
                 <h1>{todo.title}</h1>
                 <h2>{todo.description}</h2>
                 <button>{todo.completed == true ? "Completed" : "Mark as Complted"}</button>
