2487e8eafd23de56da87e5de4eace901bd205a86
Aditya Pratap Singh <66251570+aditorito@users.noreply.github.com>
2024-08-18 20:14:05 +0530

    removed my personal mongoose llink

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { string } = require('zod');
 
-mongoose.connect('mongodb+srv://AdityaPratap:qoYeY20Bkg1E45rK@cluster0.p5yi9c8.mongodb.net/todo-app');
+mongoose.connect('your-mongoose-link');
 
 
 const todoSchema = new mongoose.Schema({
